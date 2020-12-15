6c9a3d98cb79135f8d8dced4e9be223348722746
patel <parthpatel167@gmail.com>
2020-12-14 18:56:16 -0600

    background image updated with some styling with font for main page and all adding ,deleting on clickfunction for burgers js updated

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,37 @@
+$(function () {
+    //Click event to eat a burger.
+    $(".devoure-burger").on("click", function (event) {
+      console.log("click working");
+      var id = $(this).data("id");
+  
+      // Send the DELETE request using ajax.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+      }).then(function () {
+        console.log("devoured burger", id);
+        // Reload the page to get the updated list
+        location.reload();
+      });
+    });
+    $(".create-form").on("submit", function (event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+      console.log("submit button hit");
+  
+      //Grab burger name from form field.
+      var newBurger = {
+        burger_name: $("#newBurger").val().trim(),
+        devoured: 0,
+      };
+  
+      // Send the POST request using ajax.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger,
+      }).then(function () {
+        console.log("created new burger");
+        // Reload page
+        location.reload();
+      });
+    });
+  });
