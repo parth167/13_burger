c2b8d337fc4237dc7492cebb41c03520358ed9ec
patel <parthpatel167@gmail.com>
2020-12-14 18:46:02 -0600

    table created in schema file

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+burger_name VARCHAR(255) NOT NULL,
+devoured BOOLEAN DEFAULT false
+);
