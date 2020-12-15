dc89ce45f158f7231e430a1d39654758108efe24
patel <parthpatel167@gmail.com>
2020-12-14 18:47:13 -0600

    data inserted

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+SELECT * FROM burgers_db.burgers;
+insert INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon-Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie-Burger', true);
