11946f7353aada80fd857f8c171cc920b6d406f0
patel <parthpatel167@gmail.com>
2020-12-15 18:29:29 -0600

    few chnges

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 const mysql = require('mysql');
-
-const connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+ connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Parth@167",
-    database: "burgers_db",
+    password: "hacktheolanet",
+    database: "todoagain_db",
 });
-
+};
 connection.connect(function(err) {
     if (err) {
         console.log("error connecting: " + err.stack);
@@ -15,5 +18,5 @@ connection.connect(function(err) {
     }
     console.log("connected as id " + connection.threadId);
 });
-
+connection.connect();
 module.exports = connection;
