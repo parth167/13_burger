a5e3810089f8287b7919d02533273039f916d66e
patel <parthpatel167@gmail.com>
2020-12-15 19:59:58 -0600

    few changes

--- config/connection.js
+++ config/connection.js
@@ -5,18 +5,12 @@ if (process.env.JAWSDB_URL){
 }else{
  connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+  
     user: "root",
     password: "hacktheolanet",
     database: "todoagain_db",
 });
 };
-connection.connect(function(err) {
-    if (err) {
-        console.log("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+
 connection.connect();
 module.exports = connection;
